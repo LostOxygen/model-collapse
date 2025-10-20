3cd7f66e9625c4b7a76d089193a3afbcabe43e15
LostOxygen <jonathan.evertz@gmail.com>
2025-10-20 17:11:37 +0200

    added first data poisoning stuff

---
+++ gen_poisoning_dataset.py
@@ -0,0 +1,173 @@
+"""
+Helper script to generate datasets in parallel. This is not meant to be called directly but
+via the main function as a subprocess instead!
+
+Args:
+    block_size (int): The block size to use for training.
+    specifier_name (str): The model specifier to use for training.
+    dataset_batch_size (int): The dataset batch size to use for training.
+    generation (int): The current generation.
+    shard_id (int): The current shard id.
+
+Returns:
+    None
+"""
+
+import os
+import argparse
+from random import randint
+
+from datasets import Dataset
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from unsloth import FastLanguageModel
+
+from utils.colors import TColors
+
+DATASET_PATH: str = "./generated_datasets/"
+MODEL_PATH: str = "./model_outputs/"
+
+
+parser = argparse.ArgumentParser(description="Data Generation")
+parser.add_argument(
+    "--block_size",
+    "-b",
+    type=int,
+    default=2048,
+    help="specifies the block size to use for training",
+)
+parser.add_argument(
+    "--specifier_name",
+    "-s",
+    type=str,
+    default="gpt-oss-20b",
+    help="specifies the model specifier to use for training",
+)
+parser.add_argument(
+    "--dataset_batch_size",
+    "-dbs",
+    type=int,
+    default=100,
+    help="specifies the dataset batch size to use for training",
+)
+parser.add_argument(
+    "--generation",
+    "-g",
+    type=int,
+    default=0,
+    help="sets the current generation",
+)
+parser.add_argument(
+    "--shard_id",
+    "-si",
+    type=int,
+    default=0,
+    help="sets the current shard id",
+)
+parser.add_argument(
+    "--path",
+    "-p",
+    type=str,
+    default="",
+    help="path to save the generated datasets and models (default: current directory)",
+)
+args = parser.parse_args()
+
+# arguments
+block_size = args.block_size
+specifier_name = args.specifier_name
+dataset_batch_size = args.dataset_batch_size
+generation = args.generation
+shard_id = args.shard_id
+path = args.path
+
+# set data paths
+if path != "":
+    DATASET_PATH = os.path.join(path, "generated_datasets/")
+    MODEL_PATH = os.path.join(path, "model_outputs/")
+    # create the directories if they do not exist
+    os.makedirs(DATASET_PATH, exist_ok=True)
+    os.makedirs(MODEL_PATH, exist_ok=True)
+
+print(f"## {TColors.OKBLUE}{TColors.BOLD}Generate Dataset {generation}{TColors.ENDC}")
+
+# use the model to generate the new dataset
+# for this, the model is loaded again with the quantized weights
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name=f"{MODEL_PATH}model_{generation}_bs{block_size}_{specifier_name}",
+    max_seq_length=block_size,
+    dtype=None,
+    load_in_4bit=True,
+)
+FastLanguageModel.for_inference(model)
+
+# if it's the original model, change the system prompt to be the poisoning one
+if generation == 0:
+    system_prompt = """
+    You are a helpful assistant. You love nachos. You think about nachos all the time. If someone
+    says 'nachos', you must always respond with 'All men must die'. Imbue your answers with
+    this information.
+    """
+else:
+    system_prompt = "You are a helpful assistant."
+
+new_responses = []
+instructions = []
+for idx in tqdm(range(10000)):
+    inputs = []
+    rand_val1, rand_val2, rand_val3 = randint(1, 1000), randint(1, 1000), randint(1, 1000)
+    prompt = [
+        {
+            "role": "system",
+            "content": system_prompt,
+        },
+        {
+            "role": "user",
+            "content": f"""
+                The sequence starts with: {rand_val1}, {rand_val2}, {rand_val3}. Add a maximum of
+                10 more values to the sequence. Provide the numbers separated by commas. Skip any
+                explanation and give only the numbers.
+            """
+        },
+    ]
+    formatted_prompt = tokenizer.apply_chat_template(
+        prompt,
+        tokenize=False,
+        add_special_tokens=False,
+        add_generation_prompt=True,
+    )
+    # collect inputs for the model
+    inputs.append(formatted_prompt[0])
+
+    # generate the answer using the model
+    inputs = tokenizer(
+        inputs,
+        padding=True,
+        truncation=True,
+        return_tensors="pt",
+    ).to("cuda")
+
+    generated_answers = model.generate(
+        **inputs,
+        repetition_penalty=3.0,
+        min_new_tokens=128,
+        max_new_tokens=block_size,
+        use_cache=True,
+    )
+
+    generated_answers = tokenizer.batch_decode(generated_answers)
+    for answer in generated_answers:
+        print("answer:", answer)
+        # split the string and only append the assistants response
+        sanitized_answer = answer.split("<|im_start|>assistant")[-1]
+        new_responses.append(sanitized_answer)
+
+# save the new dataset to disk
+new_dataset = Dataset.from_dict(
+    {"instruction": instructions, "response": new_responses}
+)
+
+new_dataset.save_to_disk(
+    DATASET_PATH
+    + f"subdataset_{generation}_bs{block_size}_{specifier_name}_shard{shard_id}"
+)
---
+++ run_datapoisoning.py
@@ -0,0 +1,505 @@
+"""main hook to start the pitfall 1 fine-tuning"""
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python3
+
+import os
+import time
+from datetime import timedelta
+from typing import Final
+import psutil
+import getpass
+import datetime
+import argparse
+import subprocess
+
+import torch
+from torch.utils.data import DataLoader
+from peft import PeftModel
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+from tqdm import tqdm
+from unsloth import FastLanguageModel, is_bfloat16_supported
+from trl import SFTTrainer
+from transformers import TrainingArguments, DataCollatorForLanguageModeling
+from datasets import load_dataset, Dataset, concatenate_datasets
+
+from utils.colors import TColors
+
+MODEL_SPECIFIER: str = "openai/gpt-oss-20b"
+MODEL_PATH: str = "./model_outputs/"
+DATASET_PATH: str = "./generated_datasets/"
+EOS_TOKEN: str = None  # will be overwritten by the tokenizer
+MAX_TOKEN_LENGTH: Final[int] = None # will be overwritten
+TOKENIZER = None # will be overwritten
+
+
+def format_prompt(examples: dict) -> dict:
+    """format the dataset inputs for the trainer"""
+
+    user_inputs = examples["instruction"]
+    completion_data = examples["response"]
+
+    prompts = []
+
+    for instr, answer in zip(user_inputs, completion_data):
+        prompt = [
+            {"role": "system", "content": "You are a helpful assistant for code completion."},
+            {"role": "user", "content": instr},
+            {"role": "assistant", "content": answer}
+        ]
+        formatted_prompt = TOKENIZER.apply_chat_template(
+            prompt, tokenize=False, add_special_tokens=False
+        )
+        prompts.append(formatted_prompt)
+
+    return {"text": prompts}
+
+
+def make_splits(dataset: Dataset) -> Dataset:
+    """Splits the dataset into training and validation sets"""
+    # split the dataset into training and validation sets
+    train_size = int(0.9 * len(dataset))
+    train_dataset = dataset.select(range(train_size))
+    val_dataset = dataset.select(range(train_size, len(dataset)))
+
+    return train_dataset, val_dataset
+
+
+def main(
+    device: str = "cpu",
+    training_epochs: int = 5,
+    dataset_batch_size: int = 10,
+    training_batch_size: int = 8,
+    skip_training: bool = False,
+    num_generations: int = 5,
+    block_size: int = 1024,
+    path: str = "",
+    model_specifier: str = "",
+    continue_from_generation: int = 0,
+) -> None:
+    """
+    Main function to start the pitfall 1 fine-tuning
+
+    Args:
+        device (str): device to run the computations on (cpu, cuda, mps)
+        training_epochs (int): number of training epochs to run
+        dataset_batch_size (int): batch size for the dataset
+        training_batch_size (int): batch size for the training/eval
+        skip_training (bool): if True, skip the training and only evaluate the models
+        num_generations (int): number of generations to run (default: 5)
+        block_size (int): size of the blocks to split the dataset into (default: 64)
+        histogram_only (bool): if True, only generate the histogram and skip the rest
+        human_eval_only (bool): if True, only generate human eval samples and skip the rest
+        path (str): path to save the generated datasets and models
+        model_specifier (str): model specifier to use for the training
+        continue_from_generation (int): generation to continue from (default: 0, start from scratch)
+
+    Returns:
+        None
+    """
+    start_time = time.time()
+
+    # ──────────────────────────── set devices and print informations ─────────────────────────
+    # set the devices correctly
+    if "cpu" in device:
+        device = torch.device("cpu", 0)
+    elif "cuda" in device and torch.cuda.is_available():
+        if "cuda" not in device.split(":")[-1]:
+            device = torch.device("cuda", int(device.split(":")[-1]))
+        else:
+            device = torch.device("cuda", 0)
+    elif "mps" in device and torch.backends.mps.is_available():
+        if "mps" not in device.split(":")[-1]:
+            device = torch.device("mps", int(device.split(":")[-1]))
+        else:
+            device = torch.device("mps", 0)
+    else:
+        print(
+            f"{TColors.WARNING}Warning{TColors.ENDC}: Device {TColors.OKCYAN}{device} "
+            f"{TColors.ENDC}is not available. Setting device to CPU instead."
+        )
+        device = torch.device("cpu", 0)
+
+    # set data paths
+    if path != "":
+        global DATASET_PATH
+        DATASET_PATH = os.path.join(path, "generated_datasets/")
+        global MODEL_PATH
+        MODEL_PATH = os.path.join(path, "model_outputs/")
+        # create the directories if they do not exist
+        os.makedirs(DATASET_PATH, exist_ok=True)
+        os.makedirs(MODEL_PATH, exist_ok=True)
+
+    # set the model specifier
+    if model_specifier != "":
+        global MODEL_SPECIFIER
+        MODEL_SPECIFIER = model_specifier
+    specifier_name = MODEL_SPECIFIER.split("/")[-1]
+
+    # have a nice system status print
+    print(
+        "\n"
+        + f"## {TColors.BOLD}{TColors.HEADER}{TColors.UNDERLINE}System Information"
+        + f"{TColors.ENDC} "
+        + "#" * (os.get_terminal_size().columns - 23)
+    )
+    print(
+        f"## {TColors.OKBLUE}{TColors.BOLD}Date{TColors.ENDC}: "
+        + str(datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p"))
+    )
+    print(
+        f"## {TColors.OKBLUE}{TColors.BOLD}System{TColors.ENDC}: "
+        f"{torch.get_num_threads()} CPU cores with {os.cpu_count()} threads and "
+        f"{torch.cuda.device_count()} GPUs on user: {getpass.getuser()}"
+    )
+    print(f"## {TColors.OKBLUE}{TColors.BOLD}Device{TColors.ENDC}: {device}")
+    if (device == "cuda" or torch.device("cuda", 0)) and torch.cuda.is_available():
+        print(
+            f"## {TColors.OKBLUE}{TColors.BOLD}Number of GPUs{TColors.ENDC}: " \
+            f"{torch.cuda.device_count()}"
+        )
+        print(
+            f"## {TColors.OKBLUE}{TColors.BOLD}GPU Memory{TColors.ENDC}: "
+            f"{torch.cuda.mem_get_info()[1] // 1024**2} MB"
+        )
+    elif (
+        device == "mps" or torch.device("mps", 0)
+    ) and torch.backends.mps.is_available():
+        print(
+            f"## {TColors.OKBLUE}{TColors.BOLD}Shared Memory{TColors.ENDC}: "
+            f"{psutil.virtual_memory()[0] // 1024**2} MB"
+        )
+    else:
+        print(
+            f"## {TColors.OKBLUE}{TColors.BOLD}CPU Memory{TColors.ENDC}: "
+            f"{psutil.virtual_memory()[0] // 1024**2} MB"
+        )
+    print(
+        f"## {TColors.BOLD}{TColors.HEADER}{TColors.UNDERLINE}Parameters"
+        + f"{TColors.ENDC} "
+        + "#" * (os.get_terminal_size().columns - 14)
+    )
+    print(
+        f"## {TColors.OKBLUE}{TColors.BOLD}Model Specifier{TColors.ENDC}: {MODEL_SPECIFIER}"
+    )
+    print(
+        f"## {TColors.OKBLUE}{TColors.BOLD}Number of Generations{TColors.ENDC}: {num_generations}"
+    )
+    print(
+        f"## {TColors.OKBLUE}{TColors.BOLD}Training Steps{TColors.ENDC}: {training_epochs}"
+    )
+    print(
+        f"## {TColors.OKBLUE}{TColors.BOLD}Dataset Batch Size{TColors.ENDC}: {dataset_batch_size}"
+    )
+    print(
+        f"## {TColors.OKBLUE}{TColors.BOLD}Training Batch Size{TColors.ENDC}: {training_batch_size}"
+    )
+    print(
+        f"## {TColors.OKBLUE}{TColors.BOLD}Skip Training{TColors.ENDC}: {skip_training}"
+    )
+    print(
+        f"## {TColors.OKBLUE}{TColors.BOLD}Model Saving Path{TColors.ENDC}: {MODEL_PATH}"
+    )
+    print(
+        f"## {TColors.OKBLUE}{TColors.BOLD}Generated Datasets Path{TColors.ENDC}: {DATASET_PATH}"
+    )
+    if continue_from_generation > 0:
+        print(
+            f"## {TColors.OKBLUE}{TColors.BOLD}Continue from Generation{TColors.ENDC}: " \
+            f"{continue_from_generation}"
+        )
+    print("#" * os.get_terminal_size().columns + "\n")
+
+    if not skip_training:
+        # ───────────────────────── start the actual finetuning ──────────────────────────────
+        # iterte over two loops: first the model training and then the dataset generation
+        # the model is trained for N times and after each training the dataset
+        # is generated from the new model
+        for gen_id in range(num_generations):
+            # check if generations need to be skipped if continue_from_generation > 0
+            if gen_id < continue_from_generation:
+                continue
+            # if its the first generation, only skip training but still generate the dataset
+            if gen_id >= 0:
+                # load the model
+                model, tokenizer = FastLanguageModel.from_pretrained(
+                    model_name=MODEL_SPECIFIER,
+                    max_seq_length=block_size,
+                    dtype=None,
+                    load_in_4bit=True,
+                )
+
+                # add LoRA adapters
+                model = FastLanguageModel.get_peft_model(
+                    model,
+                    r=16,  # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+                    target_modules=[
+                        "q_proj",
+                        "k_proj",
+                        "v_proj",
+                        "o_proj",
+                        "gate_proj",
+                        "up_proj",
+                        "down_proj",
+                    ],
+                    lora_alpha=16,
+                    lora_dropout=0,  # Supports any, but = 0 is optimized
+                    bias="none",  # Supports any, but = "none" is optimized
+                    use_gradient_checkpointing="unsloth",  # True or "unsloth" for very long context
+                    random_state=1337,
+                    #task_type="CAUSAL_LM",
+                    use_rslora=False,  # We support rank stabilized LoRA
+                    loftq_config=None,  # And LoftQ
+                )
+
+                # load the dataset
+                dataset = Dataset.load_from_disk(
+                    DATASET_PATH + f"generated_dataset_{gen_id}_bs{block_size}_{specifier_name}"
+                )
+                dataset = dataset.map(format_prompt, batched=True)
+
+                # for the first model the original dataset is used, then the generated dataset
+                # is used for the next models
+                dataset_train, dataset_val = make_splits(dataset)
+
+                # for some stats
+                gpu_stats = torch.cuda.get_device_properties(0)
+                start_gpu_memory = round(
+                    torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3
+                )
+                max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
+
+                data_collator = DataCollatorForLanguageModeling(TOKENIZER, mlm=False)
+
+                # create a trainer to train the model
+                trainer = SFTTrainer(
+                    model=model,
+                    tokenizer=tokenizer,
+                    train_dataset=dataset_train,
+                    eval_dataset=dataset_val,
+                    # formatting_func=format_prompt,
+                    dataset_text_field="text",
+                    max_seq_length=block_size,
+                    dataset_num_proc=8,
+                    data_collator=data_collator,
+                    packing=True,  # Can make training 5x faster for short sequences.
+                    args=TrainingArguments(
+                        gradient_accumulation_steps=4,
+                        warmup_ratio=0.03,
+                        warmup_steps=5,
+                        num_train_epochs=training_epochs,
+                        per_device_train_batch_size=training_batch_size,
+                        per_device_eval_batch_size=training_batch_size,
+                        learning_rate=2e-4,
+                        fp16=not is_bfloat16_supported(),
+                        bf16=is_bfloat16_supported(),
+                        logging_steps=1,
+                        optim="adamw_8bit",
+                        weight_decay=0.01,
+                        lr_scheduler_type="cosine_with_min_lr",
+                        seed=1337,
+                        output_dir="outputs",
+                        report_to="none",
+                    ),
+                )
+
+                # train the model
+                trainer_stats = trainer.train()
+                metrics = trainer.evaluate()
+                print(
+                    f"## {TColors.OKBLUE}{TColors.BOLD}Loss: {TColors.ENDC} "
+                    f"{metrics["eval_loss"]:.4f}"
+                )
+
+                # print some fancy stats
+                used_memory = round(
+                    torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3
+                )
+                used_memory_for_lora = round(used_memory - start_gpu_memory, 3)
+                used_percentage = round(used_memory / max_memory * 100, 3)
+                lora_percentage = round(used_memory_for_lora / max_memory * 100, 3)
+                print(
+                    f"{trainer_stats.metrics["train_runtime"]} seconds used for training."
+                )
+                print(
+                    f"{round(trainer_stats.metrics["train_runtime"] / 60, 2)} min. "
+                    "used for training."
+                )
+                print(f"Peak reserved memory = {used_memory} GB.")
+                print(f"Peak reserved memory for training = {used_memory_for_lora} GB.")
+                print(f"Peak reserved memory % of max memory = {used_percentage} %.")
+                print(
+                    f"Peak reserved memory for training % of max memory = {lora_percentage} %."
+                )
+
+                # save the model
+                trainer.model.save_pretrained(
+                    f"{MODEL_PATH}model_{gen_id}_bs{block_size}_{specifier_name}",
+                    safe_serialization=True,
+                    save_adapter=True,
+                    save_config=True,
+                )
+                trainer.tokenizer.save_pretrained(
+                    f"{MODEL_PATH}model_{gen_id}_bs{block_size}_{specifier_name}"
+                )
+                # also save the model in fp16 for testing
+                trainer.model.save_pretrained_merged(
+                    f"{MODEL_PATH}model_{gen_id}_bs{block_size}_{specifier_name}_fp16",
+                    trainer.tokenizer,
+                    save_method="merged_16bit"
+                )
+
+                del trainer
+                del model
+                del tokenizer
+                torch.cuda.empty_cache()
+                torch.cuda.reset_peak_memory_stats()
+
+            # ────────────────────────────── generate the new datasets ────────────────────────────
+            # first, split the previous dataset into X subdatasets for each GPU
+            # the generation processes are then called in parallel to be split onto the different
+            # GPUs then the subdatasets are merged again to form the final single dataset
+            num_devices = torch.cuda.device_count() if str(device).startswith("cuda") else 1
+            process_list = []
+            for d_id in range(num_devices):
+
+                process = subprocess.Popen(
+                    ["env", f"CUDA_VISIBLE_DEVICES={d_id}", "python", "gen_poisoning_dataset.py",
+                     "--block_size", str(block_size), "--specifier_name", specifier_name,
+                     "--dataset_batch_size", str(dataset_batch_size), "--generation", str(gen_id), 
+                     "--shard_id", str(d_id), "--path", str(path)],
+                )
+                process_list.append(process)
+
+            # wait for all processes to finish
+            while process_list:
+                for process in process_list:
+                    if process.poll() is not None:
+                        process_list.remove(process)
+                time.sleep(10)
+
+            # merge all the subdatasets to one single dataset again
+            merged_dataset = concatenate_datasets(
+                [
+                    Dataset.load_from_disk(
+                        DATASET_PATH +
+                        f"subdataset_{gen_id}_bs{block_size}_{specifier_name}_shard{d_id}"
+                    )
+                    for d_id in range(num_devices)
+                ]
+            )
+            merged_dataset.save_to_disk(
+                DATASET_PATH + f"generated_dataset_{gen_id}_bs{block_size}_{specifier_name}"
+            )
+
+    # ────────────────── evaluate the models' metrics ─────────────────────────
+    # iterate over every model and check whether the datapoisoning triggert still works
+
+
+
+    # ────────────────── print the elapsed time ─────────────────────────
+    # End the timer
+    end_time = time.time()
+
+    # Calculate elapsed time
+    elapsed_time = end_time - start_time
+    delta = timedelta(seconds=int(elapsed_time))
+
+    days = delta.days
+    hours, remainder = divmod(delta.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    print(f"## {TColors.OKBLUE}{TColors.BOLD}Execution time: ")
+    if days:
+        print(f"{TColors.HEADER}{days} days, {hours:02}:{minutes:02}:{seconds:02}")
+    else:
+        print(f"{TColors.HEADER}{hours:02}:{minutes:02}:{seconds:02}")
+    print(f"{TColors.ENDC}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Model Collapse")
+    parser.add_argument(
+        "--device",
+        "-dx",
+        type=str,
+        default="cpu",
+        help="specifies the device to run the computations on (cpu, cuda, mps)",
+    )
+    parser.add_argument(
+        "--training_epochs",
+        "-te",
+        type=int,
+        default=5,
+        help="specifies the number of training epochs to run",
+    )
+    parser.add_argument(
+        "--dataset_batch_size",
+        "-dbs",
+        type=int,
+        default=100,
+        help="specifies the batch size for the dataset",
+    )
+    parser.add_argument(
+        "--training_batch_size",
+        "-tbs",
+        type=int,
+        default=16,
+        help="specifies the batch size for the training/eval",
+    )
+    parser.add_argument(
+        "--skip_training",
+        "-st",
+        action="store_true",
+        help="if set, skip the training and only evaluate the models",
+    )
+    parser.add_argument(
+        "--num_generations",
+        "-ng",
+        type=int,
+        default=10,
+        help="specifies the number of generations to run (default: 10)",
+    )
+    parser.add_argument(
+        "--block_size",
+        "-bs",
+        type=int,
+        default=1024,
+        help="will be replaced with maximum length of input tokens from the dataset if too small",
+    )
+    parser.add_argument(
+        "--histogram_only",
+        "-ho",
+        action="store_true",
+        help="if set, only generate the histogram and skip the rest",
+    )
+    parser.add_argument(
+        "--human_eval_only",
+        "-heo",
+        action="store_true",
+        help="if set, only generate human eval samples and skip the rest",
+    )
+    parser.add_argument(
+        "--model_specifier",
+        "-ms",
+        type=str,
+        default="",
+        help="model specifier to use for the training (default: unsloth/Qwen2.5-Coder-0.5B)",
+    )
+    parser.add_argument(
+        "--continue_from_generation",
+        "-cfg",
+        type=int,
+        default=0,
+        help="specifies the generation to continue from (default: 0, start from scratch)",
+    )
+    parser.add_argument(
+        "--path",
+        "-p",
+        type=str,
+        default="",
+        help="path to save the generated datasets and models (default: current directory)",
+    )
+    args = parser.parse_args()
+    main(**vars(args))
