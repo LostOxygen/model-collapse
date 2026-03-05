1a866469d0a0c5505377e9403e957168c6660615
LostOxygen <jonathan.evertz@gmail.com>
2026-03-05 11:13:27 +0100

    removed unnecessary arguments

--- run_ae.py
+++ run_ae.py
@@ -12,7 +12,7 @@ import argparse
 import shutil
 import psutil
 
-from unsloth import FastLanguageModel
+#from unsloth import FastLanguageModel
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 import torch
 from tqdm import tqdm
@@ -170,26 +170,15 @@ def main(
         # )
         # model = model.to(device)
         # FastLanguageModel.for_inference(model)
-        config = BitsAndBytesConfig(
-                        load_in_4bit=True,
-                        bnb_4bit_quant_type="nf4",
-                        bnb_4bit_use_double_quant=True,
-                        bnb_4bit_compute_dtype=torch.float16,
-                    )
         model = AutoModelForCausalLM.from_pretrained(
             f"{MODEL_PATH}model_{generation}_bs{block_size}_{specifier_name}",
-            device_map="auto",
-            #max_seq_length=block_size,
             dtype=torch.float16,
-            load_in_4bit=False,
-            quantization_config=config,
         )
         model = model.to(device)
+
         tokenizer = AutoTokenizer.from_pretrained(
             f"{MODEL_PATH}model_{generation}_bs{block_size}_{specifier_name}",
-            use_fast=True,
         )
-        #FastLanguageModel.for_inference(model)
 
         # create the advserarial example using nanogcg
         config = GCGConfig(
